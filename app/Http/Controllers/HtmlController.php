0aa0e4e862a814a1f745f74b2c5b3a3ef5babcd6
Dylan <nlan4670@gmail.com>
2025-03-28 01:58:55 +0700

    create EXE

---
+++ app/Http/Controllers/HtmlController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+class HtmlController extends Controller
+{
+    public function login()
+    {
+        return file_get_contents(public_path('html/login.html'));
+    }
+
+    public function register()
+    {
+        return file_get_contents(public_path('html/register.html'));
+    }
+    public function list()
+    {
+        return file_get_contents(public_path('html/list.html'));
+    }
+    public function update()
+    {
+        return file_get_contents(public_path('html/update.html'));
+    }
+    public function view()
+    {
+        return file_get_contents(public_path('html/view.html'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CrudUserController;
 use App\Http\Controllers\SocialiteController;
+use App\Http\Controllers\HtmlController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,38 @@ use App\Http\Controllers\SocialiteController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::controller(SocialiteController::class)->group(function () {
+    Route::controller(SocialiteController::class)->group(function () {
     Route::get('auth/google','googleLogin')->name('auth.google');
 Route::get('auth/google-callback','googleAuthenication')->name('auth.google-callback');
 });
 
+Route::get('/login1', [HtmlController::class, 'login']);
+Route::get('/register', [HtmlController::class, 'register']);
+Route::get('/list1', [HtmlController::class, 'list']);
+Route::get('/update1', [HtmlController::class, 'update']);
+Route::get('/view', [HtmlController::class, 'view']);
 
 
+
+Route::get('/login1', function () {
+    return file_get_contents(public_path('EXE/EXE1/public/login.html'));
+});
+
+Route::get('/register', function () {
+    return file_get_contents(public_path('EXE/EXE1/public/register.html'));
+});
+
+Route::get('/list1', function () {
+    return file_get_contents(public_path('EXE/EXE1/public/list1.html'));
+});
+
+Route::get('/update1', function () {
+    return file_get_contents(public_path('EXE/EXE1/public/update.html'));
+});
+
+Route::get('/view', function () {
+    return file_get_contents(public_path('EXE/EXE1/public/view.html'));
+});
 
 Route::get('dashboard', [CrudUserController::class, 'dashboard']);
 
@@ -37,7 +63,7 @@ Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.dele
 Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
 Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
 
-Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
+// Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 
 Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
 
